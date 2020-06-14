1915f3ec44b81959a1e229230da521639f979780
Herciu Alexandru <sandu@opsguru.io>
2020-06-14 12:44:43 +0300

    port 8181

--- src/app.py
+++ src/app.py
@@ -17,10 +17,13 @@ def cpustress():                      # call method cpustress
         command = shlex.split(command)
         process = subprocess.Popen(command, stdout = subprocess.PIPE)
         return "running a cpu stress test for 60s"
+@app.route("/healthz")            # pod health check for /healthz 
+def healthz():                    # 
+    return "OK"                   # which returns "OK" and expecting a 200
 
-@app.route("/test")                   # at the end point /test 
-def test():                      # call method hello
-    return "Success"         # which returns "success"
+@app.route("/test")               # at the end point /test 
+def test():                       # call method hello
+    return "Success"              # which returns "success"
 
 if __name__ == "__main__":        # on running python app.py
-    app.run(host="0.0.0.0",port='8181')                     # run the flask app
+    app.run(host="0.0.0.0",port='8181')                     # run the flask app on all interfaces on port 8181
